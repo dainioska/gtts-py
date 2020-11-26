4bc57a9d3dc2674194527615d61684e7478fafa5
dainioshka <dainioshka@gmail.com>
2020-11-26 11:37:30 +0200

    commit>add gtts_test.py

---
+++ gtts_test.py
@@ -0,0 +1,15 @@
+#gtts testas 2020-11-26 vscode done
+
+from gtts import gTTS
+from playsound import playsound
+
+def play_audio(audio):
+    playsound(audio)
+
+def convert_to_audio(text):
+    audio =gTTS(text)
+    audio.save("textaudio.mp3")
+    play_audio("textaudio.mp3")
+
+convert_to_audio("laba diena dainiau")
+
